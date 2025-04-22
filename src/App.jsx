5a9ea82cbf6600b042da86e4537bea2aadcc7229
bean <sksekdh@gmail.com>
2025-04-22 20:56:46 +0900

    make settings

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Hello Webpack + Babel + React!</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // output 디렉토리 정리
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // import 시 확장자 생략 허용
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  mode: 'development',
+};
