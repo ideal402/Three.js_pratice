c5097fee1e41bdd26eb20a1fcaf1155c8f22a89f
bean <sksekdh@gmail.com>
2025-04-23 18:55:34 +0900

    create three.js obj

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+// App.js
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
 
-const App = () => {
-  return <h1>Hello Webpack + Babel + React!</h1>;
-};
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="skyblue" />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <Box />
+      </Canvas>
+    </div>
+  );
+}
 
 export default App;
